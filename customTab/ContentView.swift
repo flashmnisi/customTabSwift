4be9cf7226077e649bebfd67af29d339428dfbfd
flashmnisi <flashmnisi@gmail.com>
2024-06-20 11:09:39 +0200

    first commit

--- customTab/ContentView.swift
+++ customTab/ContentView.swift
@@ -2,20 +2,31 @@
 //  ContentView.swift
 //  customTab
 //
-//  Created by flash mnisi on 2024/06/20.
+//  Created by flash mnisi mvula on 2024/06/20.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tabSelection = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView(selection: $tabSelection) {
+            Text("Tab Content 1")
+                .tag(1)
+            Text("Tab Content 2")
+                .tag(2)
+            Text("Tab Content 3")
+                .tag(3)
+            Text("Tab Content 4")
+                .tag(4)
+            Text("Tab Content 7")
+                .tag(5)
+           
         }
-        .padding()
+        .overlay(alignment: .bottom) {
+            customTabView(tabSelection: $tabSelection)
+        }
     }
 }
 
--- customTab/customTabView.swift
+++ customTab/customTabView.swift
@@ -8,11 +8,62 @@
 import SwiftUI
 
 struct customTabView: View {
+    @Binding var tabSelection: Int
+    @Namespace private var animationNamespace
+    
+    let tabBarItems: [(image: String ,title: String)] = [
+        ("house", "Home"),
+        ("magnifyingglass", "Search"),
+        ("heart", "Favourates"),
+        ("person", "Profile"),
+        ("gear", "Settings")
+    ]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Capsule()
+                .frame(height: 80)
+                .foregroundColor(Color(.secondarySystemBackground))
+                .shadow(radius: 2)
+            
+            HStack {
+                ForEach(0..<5) { index in
+                    Button {
+                        tabSelection = index + 1
+                    } label: {
+                        VStack(spacing: 8) {
+                            Spacer()
+                            
+                            Image(systemName: tabBarItems[index].image)
+                            Text(tabBarItems[index].title)
+                                .font(.caption)
+                            
+                            if index + 1 == tabSelection {
+                                Capsule()
+                                    .frame(height: 8)
+                                    .foregroundColor(.red)
+                                    .matchedGeometryEffect(id: "SelectedTabId", in: animationNamespace)
+                                    .offset(y:3)
+                            } else {
+                                Capsule()
+                                    .frame(height: 8)
+                                    .foregroundColor(.clear)
+                                    .offset(y:3)
+                            }
+                        }
+                        .foregroundColor(index + 1 == tabSelection ? .blue : .gray)
+                    }
+
+                }
+            }
+            .frame(height: 80)
+            .clipShape(Capsule())
+        }
+        .padding(.horizontal)
     }
 }
 
 #Preview {
-    customTabView()
+    customTabView(tabSelection: .constant(1))
+        .previewLayout(.sizeThatFits)
+        .padding(.vertical)
 }
