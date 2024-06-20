8f77cc9e1d7e1d70436ac8b16c037b5adae821c7
flashmnisi <flashmnisi@gmail.com>
2024-06-20 07:53:17 +0200

    Initial Commit

---
+++ customTab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  customTab
+//
+//  Created by flash mnisi on 2024/06/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ customTab/customTabApp.swift
@@ -0,0 +1,17 @@
+//
+//  customTabApp.swift
+//  customTab
+//
+//  Created by flash mnisi on 2024/06/20.
+//
+
+import SwiftUI
+
+@main
+struct customTabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
