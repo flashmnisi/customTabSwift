62e050a5d5d5e22246776b828749297fc5f2a104
flashmnisi <flashmnisi@gmail.com>
2024-06-20 10:48:22 +0200

    added tabs

---
+++ customTab/customTabView.swift
@@ -0,0 +1,18 @@
+//
+//  customTabView.swift
+//  customTab
+//
+//  Created by flash mnisi on 2024/06/20.
+//
+
+import SwiftUI
+
+struct customTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    customTabView()
+}
